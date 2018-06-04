e41666a2c61ff26c5932cbcd5ff5b72c2024a5de
Goor Lavi <goorlavi1@gmail.com>
2018-06-05 01:23:33 +0300

    Done

--- ASPTest/BL/EventsRepository.cs
+++ ASPTest/BL/EventsRepository.cs
@@ -1,4 +1,4 @@
-﻿using ASPTest.BL;
+using ASPTest.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +9,69 @@ namespace ASPTest
     public class EventsRepository : IEvents
     {
         IData _DataRepository = new DataRepository();
-        List<int> Items = new List<int>();
+        ListData listData = ListData.Instance;
         Random _rand = new Random();
+
         public EventsRepository()
         {
-           
-            for (int i = 0; i < 100; i++)
-            {
-                Items.Add(_rand.Next(0, 100));
-            }
 
-            _DataRepository.SaveItems(Items);
+            // If list did not initialized yet
+            // generate data and save into singleton
+            if (listData.Items == null)
+            {
+
+                // Used both list and beDirectional dictionary
+                // first initial will be slow but its more efficient at run time
+                var items = new List<int>();
+                var itemsPositions = new Dictionary<int, Dictionary<int, int>>();
+                var itemsPositionsInvers = new Dictionary<int, Dictionary<int, int>>();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    var randomNumber = _rand.Next(0, 100);
+                    items.Add(randomNumber);
+
+                    if (!itemsPositions.ContainsKey(randomNumber))
+                    {
+                        itemsPositions.Add(randomNumber, new Dictionary<int, int>());
+                        itemsPositionsInvers.Add(randomNumber, new Dictionary<int, int>());
+                    }
+
+                    var currentRepeatNumber = itemsPositions[randomNumber].Count;
+                    itemsPositions[randomNumber].Add(i, currentRepeatNumber);
+                    itemsPositionsInvers[randomNumber].Add(currentRepeatNumber, i);
+                }
+
+                //_DataRepository.SaveItems(items); // Was not needed at first stage
+                listData.InitItems(items, itemsPositions, itemsPositionsInvers);
+            }
         }
 
         public int Next(int Postion)
         {
-            return 0;
+            if (listData.Items.Count > Postion && Postion > 0)
+            {
+                var number = listData.Items[Postion];
+
+                if (listData.ItemsPositions.ContainsKey(number))
+                {
+                    var currentRepeatNumber = listData.ItemsPositions[number][Postion];
+
+                    if (listData.ItemsPositionsInvers[number].ContainsKey(currentRepeatNumber + 1))
+                    {
+                        return listData.ItemsPositionsInvers[number][currentRepeatNumber + 1];
+                    }
+                }
+            }
+
+            // Case there is no number next repeat
+            // or Position is not valid
+            return -1;
         }
 
         public List<int> GetAllItems()
         {
-            return Items;
+            return listData.Items;
         }
     }
 
